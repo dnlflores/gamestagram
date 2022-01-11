c70766273b95e9bcb1c18473fe4ae109819e5605
Jared B <jburnt17@gmail.com>
2022-01-10 23:48:16 -0500

    added nav-bar styles

---
+++ react-app/src/components/auth/LogOutButton/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../../store/session';
+import './LogOutButton.css'
+
+const LogoutButton = () => {
+  const dispatch = useDispatch()
+  const onLogout = async (e) => {
+    await dispatch(logout());
+  };
+
+  return <button className="logout-button" onClick={onLogout}>Logout</button>;
+};
+
+export default LogoutButton;
