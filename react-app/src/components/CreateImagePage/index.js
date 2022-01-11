c479681f376c6ce624e020617376ae481edb115e
Jared B <jburnt17@gmail.com>
2022-01-11 00:50:48 -0500

    added login styles

--- react-app/src/components/CreateImagePage/index.js
+++ react-app/src/components/CreateImagePage/index.js
@@ -1,71 +1,69 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-
+import NavBar from "../Navbar";
 
 const CreateImagePage = () => {
-    const history = useHistory(); // so that we can redirect after the image upload is successful
-    const [errors, setErrors] = useState([]);
-    const [image, setImage] = useState(null);
-    const [imageLoading, setImageLoading] = useState(false);
-    const [caption, setCaption] = useState('');
-    
-    
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const formData = new FormData();
-        formData.append("image", image);
-        
-        // aws uploads can be a bit slow—displaying
-        // some sort of loading message is a good idea
-        setImageLoading(true);
+  const history = useHistory(); // so that we can redirect after the image upload is successful
+  const [errors, setErrors] = useState([]);
+  const [image, setImage] = useState(null);
+  const [imageLoading, setImageLoading] = useState(false);
+  const [caption, setCaption] = useState("");
 
-        formData.append("caption", caption)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("image", image);
 
-        const res = await fetch('/api/games', {
-            method: "POST",
-            body: formData,
-        });
-        if (res.ok) {
-            await res.json();
-            setImageLoading(false);
-            history.push("/games");
-        }
-        else {
-            setImageLoading(false);
-            // a real app would probably use more advanced
-            // error handling
-            console.log("error");
-        }
+    // aws uploads can be a bit slow—displaying
+    // some sort of loading message is a good idea
+    setImageLoading(true);
+
+    formData.append("caption", caption);
+
+    const res = await fetch("/api/games", {
+      method: "POST",
+      body: formData,
+    });
+    if (res.ok) {
+      await res.json();
+      setImageLoading(false);
+      history.push("/games");
+    } else {
+      setImageLoading(false);
+      // a real app would probably use more advanced
+      // error handling
+      console.log("error");
     }
-    
-    const updateImage = (e) => {
-        const file = e.target.files[0];
-        setImage(file);
-    }
+  };
 
-    const updateCaption = (e) => {
-        setCaption(e.target.value);
-      };
-    
-    return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label>Caption</label>
-                <input type='text'
-                        name='caption'
-                        onChange={updateCaption}
-                        value={caption}
-                        ></input>
-            </div>
-            <input
-              type="file"
-              accept="image/*"
-              onChange={updateImage}
-            />
-            <button type="submit">Submit</button>
-            {(imageLoading)&& <p>Loading...</p>}
-        </form>
-    )
-}
+  const updateImage = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+  };
+
+  const updateCaption = (e) => {
+    setCaption(e.target.value);
+  };
+
+  return (
+    <>
+      <NavBar />
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Caption</label>
+          <input
+            type="text"
+            name="caption"
+            onChange={updateCaption}
+            value={caption}
+          ></input>
+        </div>
+        <input type="file" accept="image/*" onChange={updateImage} />
+        <button type="submit">Submit</button>
+        {imageLoading && <p>Loading...</p>}
+      </form>
+    </>
+  );
+};
 
 export default CreateImagePage;
---
+++ react-app/src/components/auth/LoginForm/index.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { NavLink, Redirect } from "react-router-dom";
+import { login } from "../../../store/session";
+import './LoginForm.css'
+
+const LoginForm = () => {
+  const [errors, setErrors] = useState([]);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const user = useSelector((state) => state.session.user);
+  const dispatch = useDispatch();
+
+  const onLogin = async (e) => {
+    e.preventDefault();
+    const data = await dispatch(login(email, password));
+    if (data) {
+      setErrors(data);
+    }
+  };
+
+  const updateEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const updatePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  if (user) {
+    return <Redirect to="/" />;
+  }
+
+  return (
+    <div className="login-body">
+      <h1 className="login-title">Gamestagram</h1>
+      <img className="login-image" src=""></img>
+      <form className="login-form" onSubmit={onLogin}>
+        <div>
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
+        </div>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={updateEmail}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={updatePassword}
+        />
+        <button type="submit">Login</button>
+      </form>
+      <div className="login-signup-con">
+        Don't have an account? <NavLink to="/sign-up">Sign Up</NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
