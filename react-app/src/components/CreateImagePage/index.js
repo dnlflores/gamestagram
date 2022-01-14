e2cc62097898dc6771cfc0233dbb51824d5a9f4d
Chloe-Baylock <chloebox523@gmail.com>
2022-01-14 14:08:18 -0500

    just some commit

--- react-app/src/components/CreateImagePage/index.js
+++ react-app/src/components/CreateImagePage/index.js
@@ -1,58 +1,21 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
 import NavBar from "../Navbar";
 import SelectImagePage from "../SelectImagePage"
 import './CreateImagePage.css'
 
 const CreateImagePage = () => {
-  const history = useHistory(); // so that we can redirect after the image upload is successful
   // const [errors, setErrors] = useState([]);
-  const [image, setImage] = useState(null);
-  const [imageLoading, setImageLoading] = useState(false);
-  const [caption, setCaption] = useState("");
   const [imageSelectPopup, setImageSelectPopup] = useState(false);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append("image", image);
-
-    // aws uploads can be a bit slow—displaying
-    // some sort of loading message is a good idea
-    setImageLoading(true);
-
-    formData.append("caption", caption);
-
-    const res = await fetch("/api/games", {
-      method: "POST",
-      body: formData,
-    });
-    if (res.ok) {
-      await res.json();
-      setImageLoading(false);
-      history.push("/games");
-    } else {
-      setImageLoading(false);
-      // a real app would probably use more advanced
-      // error handling
-      console.log("there was an error here is some info", res, res.formData, res.status);
-    }
-  };
-
-  const updateImage = (e) => {
-    const file = e.target.files[0];
-    setImage(file);
-  };
-
-  const updateCaption = (e) => {
-    setCaption(e.target.value);
-  };
 
   return (
     <>
       <NavBar />
       <header className="grid">
         <div className="grid-mid">
+          <SelectImagePage
+            trigger={imageSelectPopup}
+            setTrigger={setImageSelectPopup}
+          />
           <p>choose an image to upload!</p>
           <button
             className="modal-button"
@@ -65,26 +28,12 @@ const CreateImagePage = () => {
             +
           </button>
           <p>set caption</p>
-          <SelectImagePage
-            trigger={imageSelectPopup}
-            setTrigger={setImageSelectPopup}
-          />
+        </div>
+        <div className="ender" onClick={e => setImageSelectPopup(false)}>
+          <p> click here to exit modal </p>
         </div>
       </header>
-      {/* <form onSubmit={handleSubmit}>
-        <div>
-          <label>Caption</label>
-          <input
-            type="text"
-            name="caption"
-            onChange={updateCaption}
-            value={caption}
-          ></input>
-        </div>
-        <input type="file" accept="image/*" onChange={updateImage} />
-        <button type="submit">Submit</button>
-        {imageLoading && <p>Loading...</p>}
-      </form> */}
+     
 
     </ >
   );
--- react-app/src/components/SelectImagePage/index.js
+++ react-app/src/components/SelectImagePage/index.js
@@ -1,6 +1,48 @@
-
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const SelectImagePage = (props) => {
+
+    const [image, setImage] = useState(null);
+    const [imageLoading, setImageLoading] = useState(false);
+    const [caption, setCaption] = useState("");
+    const history = useHistory(); // so that we can redirect after the image upload is successful
+
+    const updateImage = (e) => {
+        const file = e.target.files[0];
+        setImage(file);
+    };
+
+    const updateCaption = (e) => {
+        setCaption(e.target.value);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append("image", image);
+
+        // aws uploads can be a bit slow—displaying
+        // some sort of loading message is a good idea
+        setImageLoading(true);
+
+        formData.append("caption", caption);
+
+        const res = await fetch("/api/games", {
+            method: "POST",
+            body: formData,
+        });
+        if (res.ok) {
+            await res.json();
+            setImageLoading(false);
+            history.push("/games");
+        } else {
+            setImageLoading(false);
+            // a real app would probably use more advanced
+            // error handling
+            console.log("there was an error here is some info", res, res.formData, res.status);
+        }
+    };
 
     return (
         <div
@@ -10,11 +52,22 @@ const SelectImagePage = (props) => {
         // }}
         >
             {props.trigger && (
-                <div className="modal-grid">
-                    <div className="modal-outer">
-                        <div className="modal-inner">
-                            <p>you win!</p>
-                        </div>
+                <div className="modal-outer">
+                    <div className="modal-inner">
+                        <form onSubmit={handleSubmit}>
+                            <input type="file" accept="image/*" onChange={updateImage} />
+                            <div className="upload">
+                                <label>Caption</label>
+                                <input
+                                    type="text"
+                                    name="caption"
+                                    onChange={updateCaption}
+                                    value={caption}
+                                ></input>
+                            </div>
+                            <button type="submit">Submit</button>
+                            {imageLoading && <p>Loading...</p>}
+                        </form>
                     </div>
                 </div>
             )}
